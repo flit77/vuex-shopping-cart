3e08d248e5964d5be8db0b639dd863d492caa4b5
Vasiliy Shubko <flit77@gmail.com>
2017-10-07 23:50:20 +0200

    basic routes initialized

--- src/main.js
+++ src/main.js
@@ -1,7 +1,24 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import store from './store/index.js';
+import App from './App.vue';
+import Products from './components/Products.vue';
+import Cart from './components/Cart.vue';
+
+Vue.use(VueRouter);
+
+const routes = [
+  { path: '/', component: Products },
+  { path: '/cart', component: Cart }
+];
+
+const router = new VueRouter({
+  routes
+});
 
 new Vue({
   el: '#app',
-  render: h => h(App)
-})
+  render: h => h(App),
+  router,
+  store
+});
